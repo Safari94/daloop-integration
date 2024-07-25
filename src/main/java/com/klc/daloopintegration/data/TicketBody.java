5941dba6ca7fedf4008c653c62ac9177fce08e21
xavierrodrigues <xavier.p.rodrigues@inesctec.pt>
2024-07-25 16:06:48 +0100

    connection to Izertis

--- src/main/java/com/klc/daloopintegration/data/TicketBody.java
+++ src/main/java/com/klc/daloopintegration/data/TicketBody.java
@@ -10,15 +10,8 @@ import lombok.*;
 @ToString
 public class TicketBody {
 
-    private int problem_id;
-    private String description;
-    private int priority;
-    private int client_id;
-    private int local_id;
-    private int operator_id;
-    private String report_type;
-    private int report_id;
-    private int supplier_id;
-    private boolean include_automatic_assignees;
+    private int problemId;
+    private String stationId;
+
 
 }
--- src/main/java/com/klc/daloopintegration/services/impl/InfraspeakServiceImpl.java
+++ src/main/java/com/klc/daloopintegration/services/impl/InfraspeakServiceImpl.java
@@ -83,14 +83,14 @@ public class InfraspeakServiceImpl implements InfraspeakService {
 
     private void postToInfraspeak(String childId, String description, Integer problemId){
 
-        TicketBody ticketBody = new TicketBody(problemId,description,4,192021,Integer.parseInt(childId),0,"contact",0,0,true);
+        TicketBody ticketBody = new TicketBody(problemId,childId);
 
         RestClient restClient = RestClient.create();
         ResponseEntity<Void> response = restClient.post()
-                .uri(URI.create("https://api.infraspeak.com/v3/failures"))
+                .uri(URI.create("http://dashboard-klc.izertislabs.pt/api/maintenance/notes"))
                 .header("Accept", "application/json")
-                .header("User-Agent", "Java HttpClient")
-                .header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxNzkyIiwianRpIjoiYTFjMTA5NWI4MjY3MjZhYWZmYzMzYjA2MjBlMzA5NDI3NDUwMTkwMWE3NDMxYzgyNWMyMzBkOGU2OTRlYWI2NmRkYjUxNGE0NThmMTRlMGQiLCJpYXQiOjE3MTA0MjQyMDUsIm5iZiI6MTcxMDQyNDIwNSwiZXhwIjoyMDI1OTU3MDA1LCJzdWIiOiI5NDQxNCIsInNjb3BlcyI6W10sInV0eXBlIjoiT1BFUkFUT1IiLCJ1dHlwZV9pZCI6ODQ1NTQsIm5hbWUiOiJJbnRlZ3Jhw6fDo28gU2FsZXNmb3JjZSIsImVtYWlsIjoiaW50ZWdyYWNhbytzYWxlc2ZvcmNlQGluZnJhc3BlYWsuY29tIiwiem9uZWluZm8iOiJFdXJvcGUvTGlzYm9uIn0.hfZr5JeFKSDwFt3kfof2wh1TNnCEzmoV3pNd0Ax-I8DYRpzgz3nvx-w-FNQpr-YWck7P58vGEGp-ogWUq0RliqKoq0nZE1fwCxd0Bfc0neQiDgr6xZviow1u16uN3nAnnSmL1SQnkfW284-WoldWCHB541V2L_V8JqunukctGWNz888JM-gRwsDqpiFQG4uc0P1h4ZY9UELcW_h4d1WXR1VEPFZ2JHt1fZLids3wIeClkLr3kKX8eh0QsA0SshF_-lNFLNe0g96B4r56TSDsais14BiQhgb4R_FMDz2rFpjY7Nh37Siiw6VcBTBHBmu3ngXlmCbO0wDwpnK22yFl0PlKa8UZVsm1Fc3_OSDtuwawLYV1azZPFYAMw6JaGjDrfUiSmN2k_GzZ2VdOxHGG01cXc_0jJZ40AUmdasrIEdIy-XwW1_VvUFpgJvBkfGFHMx45HwLtWcAq7RROCt6AM2o342d3vPFR-QFVG_HunRBF3Yit3zl5t2w-43P2dQlIffcINdWyzAxh3GUe3iGTpw03ySnCEJ6FkLPuHPKVWqJxYsjeKkz82nlasKxkte1DUYNOBjN4FwiM3xZjNrAgGvEIxzaNP-JPRo2DqYV4MpBhKcolGhWK4yfty8B5LeQDO5r5C7eCcu5xkbScxxkyg_Ok2WdhH5na7_4g0OjY5RU")
+                //.header("User-Agent", "Java HttpClient")
+                //.header("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxNzkyIiwianRpIjoiYTFjMTA5NWI4MjY3MjZhYWZmYzMzYjA2MjBlMzA5NDI3NDUwMTkwMWE3NDMxYzgyNWMyMzBkOGU2OTRlYWI2NmRkYjUxNGE0NThmMTRlMGQiLCJpYXQiOjE3MTA0MjQyMDUsIm5iZiI6MTcxMDQyNDIwNSwiZXhwIjoyMDI1OTU3MDA1LCJzdWIiOiI5NDQxNCIsInNjb3BlcyI6W10sInV0eXBlIjoiT1BFUkFUT1IiLCJ1dHlwZV9pZCI6ODQ1NTQsIm5hbWUiOiJJbnRlZ3Jhw6fDo28gU2FsZXNmb3JjZSIsImVtYWlsIjoiaW50ZWdyYWNhbytzYWxlc2ZvcmNlQGluZnJhc3BlYWsuY29tIiwiem9uZWluZm8iOiJFdXJvcGUvTGlzYm9uIn0.hfZr5JeFKSDwFt3kfof2wh1TNnCEzmoV3pNd0Ax-I8DYRpzgz3nvx-w-FNQpr-YWck7P58vGEGp-ogWUq0RliqKoq0nZE1fwCxd0Bfc0neQiDgr6xZviow1u16uN3nAnnSmL1SQnkfW284-WoldWCHB541V2L_V8JqunukctGWNz888JM-gRwsDqpiFQG4uc0P1h4ZY9UELcW_h4d1WXR1VEPFZ2JHt1fZLids3wIeClkLr3kKX8eh0QsA0SshF_-lNFLNe0g96B4r56TSDsais14BiQhgb4R_FMDz2rFpjY7Nh37Siiw6VcBTBHBmu3ngXlmCbO0wDwpnK22yFl0PlKa8UZVsm1Fc3_OSDtuwawLYV1azZPFYAMw6JaGjDrfUiSmN2k_GzZ2VdOxHGG01cXc_0jJZ40AUmdasrIEdIy-XwW1_VvUFpgJvBkfGFHMx45HwLtWcAq7RROCt6AM2o342d3vPFR-QFVG_HunRBF3Yit3zl5t2w-43P2dQlIffcINdWyzAxh3GUe3iGTpw03ySnCEJ6FkLPuHPKVWqJxYsjeKkz82nlasKxkte1DUYNOBjN4FwiM3xZjNrAgGvEIxzaNP-JPRo2DqYV4MpBhKcolGhWK4yfty8B5LeQDO5r5C7eCcu5xkbScxxkyg_Ok2WdhH5na7_4g0OjY5RU")
                 .body(ticketBody)
                 .retrieve()
                 .toBodilessEntity();
